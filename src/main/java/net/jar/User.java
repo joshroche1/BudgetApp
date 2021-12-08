3639c6c28ebc77675cdae95d431885571943a6de
joshroche1 <joshua.a.roche@gmail.com>
2021-12-08 22:47:19 +0100

    Create User.java

---
+++ src/main/java/net/jar/User.java
@@ -0,0 +1,19 @@
+package net.jar;
+
+// import
+
+public class User {
+  
+  private String email;
+  private String password;
+  private String phone;
+  
+  public void setEmail(String txt) { this.email = txt; }
+  public void setPassword(String txt) { this.password = txt; }
+  public void setPhone(String txt) { this.phone = txt; }
+  
+  public String getEmail() { return this.email; }
+  public String getPassword() { return this.password; }
+  public String getPhone() { return this.phone; }
+    
+}
