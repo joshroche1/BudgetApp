25c0e572a6cdcef6ccf2de74365cc47c21679620
joshroche1 <joshua.a.roche@gmail.com>
2021-12-08 22:27:40 +0100

    Update DBUtil.java

--- src/main/java/net/jar/DBUtil.java
+++ src/main/java/net/jar/DBUtil.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpSession;
 
 public class DBUtil {
   
-  public Connection connect() {
+  private Connection connect() {
     Connection c = null;
     try {
       Class.forName("com.mysql.cj.jdbc.Driver");
@@ -28,6 +28,69 @@ public class DBUtil {
     }
     return c;
   }
+  public ResultSet select(String selectors, String tablename, String criteria) {
+    Connection c = null;
+    ResultSet rs = null;
+    try {
+      c = this.connect();
+      PreparedStatement stmt = c.prepareStatement("SELECT ? FROM ? WHERE ?",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      stmt.setString(1, selectors);
+      stmt.setString(2, tablename);
+      stmt.setString(3, critera);
+      rs = stmt.executeQuery();
+    } catch (SQLException ex) {
+      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
+    }
+    return rs;
+  }
+  public String insert(String tablename, String record) {
+    Connection c = null;
+    try {
+      c = this.connect();
+      PreparedStatement stmt = c.prepareStatement("INSERT INTO ? VALUES ?",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      stmt.setString(1,tablename);
+      stmt.setString(2,record);
+      stmt.execute();
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
+    }
+    return "";
+  }
+  public String delete(String tablename, String criteria) {
+    Connection c = null;
+    try {
+      c = this.connect();
+      PreparedStatement stmt = c.prepareStatement("DELETE FROM ? WHERE ?",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      stmt.setString(1,tablename);
+      stmt.setString(2,criteria);
+      stmt.execute();
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
+    }
+    return "";
+  }
+  public String update(String tablename, String values, String criteria) {
+    Connection c = null;
+    try {
+      c = this.connect();
+      PreparedStatement stmt = c.prepareStatement("UPDATE ? SET ? WHERE ?",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      stmt.setString(1,tablename);
+      stmt.setString(2,values);
+      stmt.setString(3,criteria);
+      stmt.execute();
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
+    }
+    return "";
+  }
+  
   
   public Boolean validateUser(String user, String pass) {
     Connection c = null;
