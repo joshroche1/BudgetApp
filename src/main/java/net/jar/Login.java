66c025c55a68d46a5636cc3a9c2731ac94898bbc
joshroche1 <joshua.a.roche@gmail.com>
2021-12-08 22:36:43 +0100

    Create Login.java

---
+++ src/main/java/net/jar/Login.java
@@ -0,0 +1,55 @@
+package net.jar;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import net.jar.DBUtil;
+import net.jar.SessionUtils;
+
+@ManagedBean(name = "beanLogin", eager = true)
+@SessionScoped
+public class Login {
+
+  private String email = "";
+  private String password = "";
+
+  public void setEmail(String em) { this.email = em;}
+  public void setPassword(String pass) { this.password = pass;}
+
+  public String getEmail() { return this.email; }
+  public String getPassword() { return this.password; }
+
+  public String getCurrentSession() {
+    String result = "";
+    HttpSession ses = SessionUtils.getSession();
+    String temp = ses.getAttribute("usertoken").toString();
+    user += "" + temp;
+    return result;
+  }
+
+  public String validateLogin() {
+    boolean valid = DAOlogin.validatePatient(username, password);
+    if (valid) {
+      HttpSession sess = SessionUtils.getSession();
+      sess.setAttribute("usertoken", email);
+      return "main";
+    } else {
+      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_WARN, "Incorrect username and/or password", "..."));
+      return "login";
+    }
+  }
+  
+  public String logout() {
+    HttpSession session = SessionUtils.getSession();
+    session.invalidate();
+    return "index";
+  }
+}
