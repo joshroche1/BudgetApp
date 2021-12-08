7c076ef4e17ff233527a6141c0ab1833e7803fef
joshroche1 <joshua.a.roche@gmail.com>
2021-12-08 22:44:39 +0100

    Create Budget.java

---
+++ src/main/java/net/jar/Budget.java
@@ -0,0 +1,28 @@
+package net.jar;
+
+// import
+
+public class Budget {
+  
+  private String name;
+  private int duedate;
+  private double value;
+  private String description;
+  private String category;
+  private String memo;
+  
+  public void setName(String txt) { this.name = txt; }
+  public void setDuedate(int dt) { this.duedate = dt; }
+  public void setValue(double val) { this.value = val; }
+  public void setDescription(String txt) { this.description = txt; }
+  public void setCategory(String txt) { this.category = txt; }
+  public void setMemo(String txt) { this.memo = txt; }
+  
+  public String getName() { return this.name; }
+  public int getDuedate() { return this.duedate; }
+  public double getValue() { return this.value; }
+  public String getDescription() { return this.description; }
+  public String getCategory() { return this.category; }
+  public String getMemo() { return this.memo; }
+  
+}
