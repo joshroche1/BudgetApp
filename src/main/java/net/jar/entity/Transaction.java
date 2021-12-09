0b35cf51845f923ba71318e288375ba37cef76ac
joshroche1 <joshua.a.roche@gmail.com>
2021-12-09 11:51:10 +0100

    Update Transaction.java

--- src/main/java/net/jar/entity/Transaction.java
+++ src/main/java/net/jar/entity/Transaction.java
@@ -1,5 +1,6 @@
 package net.jar.entity;
 
+import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -8,21 +9,21 @@ import javax.persistence.Table;
 public class Transaction {
   
   private String source;
-  private Date datetime;
+  private LocalDate datetime;
   private double value;
   private String description;
   private String category;
   private String memo;
   
   public void setSource(String txt) { this.source = txt; }
-  public void setDate(Date dt) { this.datetime = dt; }
+  public void setDate(LocalDate dt) { this.datetime = dt; }
   public void setValue(double val) { this.value = val; }
   public void setDescription(String txt) { this.description = txt; }
   public void setCategory(String txt) { this.category = txt; }
   public void setMemo(String txt) { this.memo = txt; }
   
   public String getSource() { return this.source; }
-  public Date getDate() { return this.datetime; }
+  public LocalDate getDate() { return this.datetime; }
   public double getValue() { return this.value; }
   public String getDescription() { return this.description; }
   public String getCategory() { return this.category; }
