75a5839c63130978eb0843a4959eede3216d94a6
joshroche1 <joshua.a.roche@gmail.com>
2021-12-16 16:03:07 +0100

    Update DButil.java

--- src/main/java/net/jar/DButil.java
+++ src/main/java/net/jar/DButil.java
@@ -12,69 +12,99 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpSession;
 
+import net.jar.Income;
+import net.jar.Expense;
 
 @ManagedBean(name = "beanDButil", eager = true)
 public class DButil {
 
   private Connection c = null;
   private ResultSet rs = null;
+	
+	private double incomeTotal;
+	private double expenseTotal;
+	private double remainder;
   
-  private String name;
-  private int duedate;
-  private double expected_value;
-  private double value_jan;
-  private double value_feb;
-  private double value_mar;
-  private double value_apr;
-  private double value_may;
-  private double value_jun;
-  private double value_jul;
-  private double value_aug;
-  private double value_sep;
-  private double value_oct;
-  private double value_nov;
-  private double value_dec;
-  private String description;
-  private String category;
-  private String memo;
+  private double in_jan;
+  private double in_feb;
+  private double in_mar;
+  private double in_apr;
+  private double in_may;
+  private double in_jun;
+  private double in_jul;
+  private double in_aug;
+  private double in_sep;
+  private double in_oct;
+  private double in_nov;
+  private double in_dec;
+	private double ex_jan;
+  private double ex_feb;
+  private double ex_mar;
+  private double ex_apr;
+  private double ex_may;
+  private double ex_jun;
+  private double ex_jul;
+  private double ex_aug;
+  private double ex_sep;
+  private double ex_oct;
+  private double ex_nov;
+  private double ex_dec;
   
-  public void setName(String txt) { this.name = txt; }
-  public void setDueDate(int val) { this.duedate = val; }
-  public void setExpected_value(double val) { this.expected_value = val; }
-  public void setValue_jan(double val) { this.value_jan = val; }
-  public void setValue_feb(double val) { this.value_feb = val; }
-  public void setValue_mar(double val) { this.value_mar = val; }
-  public void setValue_apr(double val) { this.value_apr = val; }
-  public void setValue_may(double val) { this.value_may = val; }
-  public void setValue_jun(double val) { this.value_jun = val; }
-  public void setValue_jul(double val) { this.value_jul = val; }
-  public void setValue_aug(double val) { this.value_aug = val; }
-  public void setValue_sep(double val) { this.value_sep = val; }
-  public void setValue_oct(double val) { this.value_oct = val; }
-  public void setValue_nov(double val) { this.value_nov = val; }
-  public void setValue_dec(double val) { this.value_dec = val; }
-  public void setDescription(String txt) { this.description = txt; }
-  public void setCategory(String txt) { this.category = txt; }
-  public void setMemo(String txt) { this.memo = txt; }
+  public void setIn_jan(double val) { this.in_jan = val; }
+  public void setIn_feb(double val) { this.in_feb = val; }
+  public void setIn_mar(double val) { this.in_mar = val; }
+  public void setIn_apr(double val) { this.in_apr = val; }
+  public void setIn_may(double val) { this.in_may = val; }
+  public void setIn_jun(double val) { this.in_jun = val; }
+  public void setIn_jul(double val) { this.in_jul = val; }
+  public void setIn_aug(double val) { this.in_aug = val; }
+  public void setIn_sep(double val) { this.in_sep = val; }
+  public void setIn_oct(double val) { this.in_oct = val; }
+  public void setIn_nov(double val) { this.in_nov = val; }
+  public void setIn_dec(double val) { this.in_dec = val; }
+  public void setEx_jan(double val) { this.ex_jan = val; }
+  public void setEx_feb(double val) { this.ex_feb = val; }
+  public void setEx_mar(double val) { this.ex_mar = val; }
+  public void setEx_apr(double val) { this.ex_apr = val; }
+  public void setEx_may(double val) { this.ex_may = val; }
+  public void setEx_jun(double val) { this.ex_jun = val; }
+  public void setEx_jul(double val) { this.ex_jul = val; }
+  public void setEx_aug(double val) { this.ex_aug = val; }
+  public void setEx_sep(double val) { this.ex_sep = val; }
+  public void setEx_oct(double val) { this.ex_oct = val; }
+  public void setEx_nov(double val) { this.ex_nov = val; }
+  public void setEx_dec(double val) { this.ex_dec = val; }
+	public void setIncomeTotal(double val) { this.incomeTotal = val; }
+	public void setExpenseTotal(double val) { this.expenseTotal = val; }
+	public void setRemainder(double val) { this.remainder = val; }
   
-  public String getName() { return this.name; }
-  public int getDueDate() { return this.duedate; }
-  public double getExpected_value() { return this.expected_value; }
-  public double getValue_jan() { return this.value_jan; }
-  public double getValue_feb() { return this.value_feb; }
-  public double getValue_mar() { return this.value_mar; }
-  public double getValue_apr() { return this.value_apr; }
-  public double getValue_may() { return this.value_may; }
-  public double getValue_jun() { return this.value_jun; }
-  public double getValue_jul() { return this.value_jul; }
-  public double getValue_aug() { return this.value_aug; }
-  public double getValue_sep() { return this.value_sep; }
-  public double getValue_oct() { return this.value_oct; }
-  public double getValue_nov() { return this.value_nov; }
-  public double getValue_dec() { return this.value_dec; }
-  public String getDescription() { return this.description; }
-  public String getCategory() { return this.category; }
-  public String getMemo() { return this.memo; }
+  public double getIn_jan() { return this.value_jan; }
+  public double getIn_feb() { return this.value_feb; }
+  public double getIn_mar() { return this.value_mar; }
+  public double getIn_apr() { return this.value_apr; }
+  public double getIn_may() { return this.value_may; }
+  public double getIn_jun() { return this.value_jun; }
+  public double getIn_jul() { return this.value_jul; }
+  public double getIn_aug() { return this.value_aug; }
+  public double getIn_sep() { return this.value_sep; }
+  public double getIn_oct() { return this.value_oct; }
+  public double getIn_nov() { return this.value_nov; }
+  public double getIn_dec() { return this.value_dec; }
+	public double getEx_jan() { return this.value_jan; }
+  public double getEx_feb() { return this.value_feb; }
+  public double getEx_mar() { return this.value_mar; }
+  public double getEx_apr() { return this.value_apr; }
+  public double getEx_may() { return this.value_may; }
+  public double getEx_jun() { return this.value_jun; }
+  public double getEx_jul() { return this.value_jul; }
+  public double getEx_aug() { return this.value_aug; }
+  public double getEx_sep() { return this.value_sep; }
+  public double getEx_oct() { return this.value_oct; }
+  public double getEx_nov() { return this.value_nov; }
+  public double getEx_dec() { return this.value_dec; }
+	public double getIncomeTotal() { return this.incomeTotal; }
+	public double getExpenseTotal() { return this.expenseTotal; }
+	public double getRemainder() { return this.remainder; }
   
   public Connection connect() {
     try {
@@ -134,12 +164,66 @@ public class DButil {
                    FacesMessage.SEVERITY_INFO, e.getMessage(), "..."));
 	  }
   }
+	
+	public double getIncomeAmount() {
+		double result = 0;
+		try {
+			DButil dbu = new DBUtil();
+			rs = dbu.getIncomes();
+			while (rs.next()) {
+				result += rs.getDouble("value");
+			}
+		} catch (SQLException ex) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
+		}
+		return result;
+	}
+	public double getExpenseAmount() {
+		double result = 0;
+		try {
+			DButil dbu = new DBUtil();
+			rs = dbu.getExpenses();
+			while (rs.next()) {
+				result += rs.getDouble("value");
+			}
+		} catch (SQLException ex) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
+		}
+		return result;
+	}
+	
+	public ResultSet getIncomes() {
+		rs = null;
+    try {
+      c = this.connect();
+      PreparedStatement stmt = c.prepareStatement("SELECT name,value,valuejan,valuefeb,valuemar,valueapr,valuemay,valuejun,valuejul,valueaug,valuesep,valueoct,valuenov,valuedec,description,memo FROM Income",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      rs = stmt.executeQuery();
+    } catch (SQLException ex) {
+      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
+    }
+    return rs;
+	}
+	public ResultSet getExpenses() {
+		rs = null;
+    try {
+      c = this.connect();
+      PreparedStatement stmt = c.prepareStatement("SELECT name,duedate,value,valuejan,valuefeb,valuemar,valueapr,valuemay,valuejun,valuejul,valueaug,valuesep,valueoct,valuenov,valuedec,description,category,memo FROM Expense",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      rs = stmt.executeQuery();
+    } catch (SQLException ex) {
+      FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
+                   FacesMessage.SEVERITY_INFO, ex.getMessage(), "..."));
+    }
+    return rs;
+	}
   
   public ResultSet getBudget() {
     rs = null;
     try {
       c = this.connect();
-      PreparedStatement stmt = c.prepareStatement("SELECT name,duedate,expected_value,value_jan,value_feb,value_mar,value_apr,value_may,value_jun,value_jul,value_aug,value_sep,value_oct,value_nov,value_dec,description,category,memo FROM Budget",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+      PreparedStatement stmt = c.prepareStatement("*",ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
       rs = stmt.executeQuery();
     } catch (SQLException ex) {
       FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(
