8ee351d47cc21b6885836d7de6340b535d0565e9
joshroche1 <joshua.a.roche@gmail.com>
2024-07-14 16:35:08 +0200

    Update

---
+++ install-deps-python.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# 
+
+apt-get update
+
+apt-get install python3 \
+ python3-dotenv \
+ python3-sqlalchemy \
+ python3-bcrypt \
+ python3-fastapi \
+ python3-jose \
+ python3-passlib \
+ sqlite3 \
+ uvicorn
